7e91e826fee9eb55af69bcdaf2a1beb9ef5bf05a
mihaisvfr <my@mail.ru>
2023-09-30 19:37:01 +0300

    tests for useris done

--- tests/test_users.py
+++ tests/test_users.py
@@ -15,7 +15,7 @@ def test_user_get_by_id(client, user):
     response = client.get('/users/1')
     assert response.status_code == 200
     assert response.json["username"] == user.username
-    assert len(response.json) != 3
+    assert response.json["role"] in ("admin", "simple_user")
 
 
 def test_user_not_found(client, user):
@@ -36,16 +36,14 @@ def test_user_creation(client):
     assert 'admin' in data.values()
 
 
-@pytest.mark.skip(reason="test not finished")
+#@pytest.mark.skip(reason="test not finished")
 def test_user_creation_already_exist(client, user):
     """
     Тест на создание пользователя с существующим именем
     """
-    # 1. Используя фикстуру user - создаем пользователя с "username": "testuser"
-    # 2. Отправляем put запрос на создание пользователя с таким же username
     user_data = {"username": "testuser", "password": "1234"}
     response = client.post('/users', json=user_data, content_type='application/json')
-    # TODO: допишите тест и запустите его, убрав декоратор @pytest.mark.skip
+    assert response.status_code == 409
 
 
 def test_user_edit(client, user, auth_headers):
@@ -60,7 +58,7 @@ def test_user_edit(client, user, auth_headers):
     assert data["username"] == user_edited_data["username"]
 
 
-@pytest.mark.skip(reason="test not implemented")
+#@pytest.mark.skip(reason="test not implemented")
 def test_user_delete(client, user, auth_headers):
-    pass
-    # TODO: реализуйте тест на удаление пользователя и запустите его, убрав декоратор @pytest.mark.skip
+    responce = client.delete(f"/users/{user.id}", headers=auth_headers)
+    assert responce.status_code == 200
--- utility/helpers.py
+++ utility/helpers.py
@@ -10,5 +10,6 @@ def not_found(e):
 def get_object_or_404(model: db.Model, object_id: int):
     object = model.query.get(object_id)
     if object is None:
-        abort(404, description=f"Author or User with id={object_id} not found")
+        object_name = model.__name__.replace('Model','')
+        abort(404, description=f"{object_name} with id={object_id} not found")
     return object
